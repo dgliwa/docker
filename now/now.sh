b919650c80fc271df87c21426052605f738be98d
Jason M. Green <Git@JasonGreen.Name>
2016-08-03 10:02:18 -0400

    Update terraform & packer versions. Update emails

--- now/now.sh
+++ now/now.sh
@@ -6,9 +6,12 @@ function now() {
 	--env TF_LOG="$TF_LOG" \
 	--env AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
 	--env AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
-	gjigsaw/now:20160417-2000 \
+	gjigsaw/now:20160803-1000 \
 	"$@"
 }
 
 alias now.terraform="now terraform"
 alias now.packer="now packer"
+
+source ~/docker/now/terraform.sh
+source ~/docker/now/packer.sh
