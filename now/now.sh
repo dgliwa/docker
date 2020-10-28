cbfcb900c36f6d49fa2b517d1165a674b42fce17
derekgliwa <dgliwa7bhs@gmail.com>
2020-10-28 12:40:31 -0700

    Tweak for myself.

--- now/now.sh
+++ now/now.sh
@@ -1,18 +1,18 @@
 function now() {
+    #    --env DOCKER_CERT_PATH="/root/.docker/machine/machines/dvb" \
+	#--env TF_LOG="$TF_LOG" \
+	#--env AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
+	#--env AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
+        #--env DOCKER_HOST="tcp://192.168.99.100:2376" \
+	#--env DOCKER_TLS_VERIFY="1" \
+	#--env DOCKER_MACHINE_NAME="dvb" \
     docker run \
 	--rm \
 	-it \
 	--volume "$(pwd):/data" \
-        --env DOCKER_CERT_PATH="/root/.docker/machine/machines/dvb" \
-	--env TF_LOG="$TF_LOG" \
-	--env AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" \
-	--env AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" \
 	--volume /var/run/docker.sock:/var/run/docker.sock \
-	--volume /Users/jigsaw/.docker:/root/.docker \
-        --env DOCKER_HOST="tcp://192.168.99.100:2376" \
-	--env DOCKER_TLS_VERIFY="1" \
-	--env DOCKER_MACHINE_NAME="dvb" \
-	gjigsaw/now:20170924-1200 \
+	--volume "/home/$(whoami)/.docker:/root/.docker" \
+	dgliwa/now:20201028 \
 	"$@"
 }
 
